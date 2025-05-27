fc310243e32931249141f7850493d77204932d1b
Philippe Mba <158060610+PhilJordan18@users.noreply.github.com>
2025-05-27 07:52:55 -0400

    git

--- src/WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ src/WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39600fcc8ca6e287ccdda00a39b4d9af8c6fae89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14de5544c8a09d039511be2e78a5f8bdbf56de00")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
